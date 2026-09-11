882ac53eb2b7aa8f221d45b43aac479712e6b8cd
Zander Roi Tabelona <ztabelona@tesda.gov.ph>
2026-09-11 18:51:58 +0800

    rename title

--- vite.config.ts
+++ vite.config.ts
@@ -81,7 +81,7 @@ function figmaSiteConfiguration(config: FigmaSiteConfiguration): Plugin {
     return html.replace(`<!-- ${slotName} -->`, content)
   }
 
-  const title = config.title ?? "Figma Make App"
+  const title = config.title ?? "Launch Pad"
   const description = config.description ?? ''
   const favicon = config.icons?.icon ?? ''
   const socialImage = config.openGraph?.image ?? ''
